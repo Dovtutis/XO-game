2e909015ddd56f4e08e5c5c3d0ffcd9dff98e6b0
unknown <dovtutis@gmail.com>
2020-11-25 15:15:40 +0200

    final

--- js/xo.js
+++ js/xo.js
@@ -8,37 +8,12 @@ const element6 = document.getElementById('6')
 const element7 = document.getElementById('7')
 const element8 = document.getElementById('8')
 const element9 = document.getElementById('9')
+const player1pointsBox = document.getElementById('player1Points')
+const player2pointsBox = document.getElementById('player2Points')
+const playerTurn = document.getElementById('playerTurn')
+
 
 let symbolTrigger = true
-let elementArray = []
-elementArray = [element1, element2, element3, element4, element5, element6, element7, element8, element9]
-
-elementArray.map(x => {
-    x.addEventListener("click", addSymbol)
-})
-
-function startNewGame(){
-    for (let i = 0; i < elementArray.length; i++) {
-        elementArray[i].innerHTML = ""
-    }
-}
-
-function addSymbol(event){
-
-    if (symbolTrigger === true) {
-        event.path[0].innerHTML = `<i class="fas fa-times bigSymbol"></i>`
-        symbolTrigger = false
-        event.target.classList.add("x")
-        checkStatus("x")
-
-    } else {
-        event.path[0].innerHTML = `<i class="far fa-circle bigSymbol"></i>`
-        symbolTrigger = true
-        event.target.classList.add("o")
-        checkStatus("o")
-    }
-}
-
 let winingConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -49,12 +24,56 @@ let winingConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+let elementArray = []
+let player1points = 0
+let player2points = 0
+
+elementArray = [element1, element2, element3, element4, element5, element6, element7, element8, element9]
+elementArray.map(x => {
+    x.addEventListener("click", addSymbol)
+})
+
+function startNewGame(){
+    for (let i = 0; i < elementArray.length; i++) {
+        elementArray[i].innerHTML = ""
+    }
+    playerTurn.innerText = "Now is CROSS player turn"
+}
+
+function addSymbol(event){
+
+    if (event.path[0].innerHTML === "") {
+        if (symbolTrigger === true) {
+            event.path[0].innerHTML = `<i class="fas fa-times bigSymbol pointerNon"></i>`
+            playerTurn.innerText = "Now is CIRCLE player turn"
+            symbolTrigger = false
+            event.target.classList.add("x")
+            checkStatus("x")
+            console.log(event)
+        } else {
+            event.path[0].innerHTML = `<i class="far fa-circle bigSymbol pointerNon"></i>`
+            symbolTrigger = true
+            playerTurn.innerText = "Now is CROSS player turn"
+            event.target.classList.add("o")
+            checkStatus("o")
+        }
+    } else {
+        console.log("pilnas")
+    }
+}
 
 function checkStatus (arg) {
     winingConditions.map(x => {
 
         if (elementArray[x[0]].classList.contains(arg) && elementArray[x[1]].classList.contains(arg) && elementArray[x[2]].classList.contains(arg)){
             alert(`Player ${arg} won`)
+            if (arg === "x") {
+                player1points++
+                player1pointsBox.innerText = `Player 1 points: ${player1points}`
+            } else {
+                player2points++
+                player2pointsBox.innerText = `Player 2 points: ${player2points}`
+            }
             startNewGame()
         }
     })
@@ -96,14 +115,6 @@ function checkStatus (arg) {
 //         startNewGame()
 //     }
 // }
-//
-//
-//
-//
-//
-//
-//
-//
 //
 
 
